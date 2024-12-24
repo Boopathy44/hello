4363edfbaa7314053d20cb0aed62eedcc84cedef
Boopathy44 <boopathyg.24cse@kongu.edu>
2024-12-24 11:57:16 +0530

    First commit

---
+++ mul.php
@@ -0,0 +1,14 @@
+<?php
+// Function to multiply two numbers
+function multiply($num1, $num2) {
+    return $num1 * $num2;
+}
+
+// Input numbers
+$num1 = 5;
+$num2 = 10;
+
+// Call the multiply function and display the result
+$result = multiply($num1, $num2);
+echo "The result of multiplying $num1 and $num2 is: $result";
+?>
